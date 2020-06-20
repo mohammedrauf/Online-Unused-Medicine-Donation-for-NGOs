d40620b34a5a04ec6571775cc1a4d25906ade6c0
mohammedrauf <mraufmirza0@gmail.com>
2020-06-20 21:04:19 +0530

    new src

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,32 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { CausesComponent } from './causes/causes.component';
-import { FcauseComponent } from './fcause/fcause.component';
-import { EventComponent } from './event/event.component';
-import { TeamComponent } from './team/team.component';
-import { CtaComponent } from './cta/cta.component';
-import { StoryComponent } from './story/story.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    HeaderComponent,
-    HomeComponent,
-    CausesComponent,
-    FcauseComponent,
-    EventComponent,
-    TeamComponent,
-    CtaComponent,
-    StoryComponent,
-    SubscribeComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
